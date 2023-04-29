bc96251a2f24c20badc9367c7897c6600022162b
kajalshinde14 <95559669+kajalshinde14@users.noreply.github.com>
2023-04-29 22:00:45 +0530

    Add files via upload

---
+++ portfolio/index.js
@@ -0,0 +1,33 @@
+/*console.log('hello world');
+
+//log,info,warn,error,table
+//log
+console.log('i am log message');
+//info
+console.info('i am info message');
+//warn
+console.warn('i am warn message');
+//error
+console.error(' i am error message');
+//table
+ let student = {
+    name : 'kajal',
+    age : 22,
+    branch : 'cse'
+ };
+    console.table(student);*/
+ //sum-----> let=keyword to declare variable
+            //a= variable name
+            //= : assignment variable
+            //10,20= value/data
+            //; = end of statement
+
+ let a = 100;
+ let b = 20;
+ let sum = a + b;
+ console.log('this is sum for a,b : '+sum);//+ =concatinate two lines,,,,join line//es5
+ //Es6 -> template strig /template literal('')back tick
+ console.log(`the sum of ${a} , ${b} is : ${sum}`);//es6
+
+ //rules in declaring variable
+ 
